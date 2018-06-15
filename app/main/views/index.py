25cf0b8bfa936abef5c427ea006f7bb0e9786f84
Arun Pottekat <apottekat@gmail.com>
2018-06-15 20:51:06 +1000

    Pagination added

--- app/main/views/index.py
+++ app/main/views/index.py
@@ -14,6 +14,13 @@ def index():
 		page_value = "1"
 
 	flag = True
+	upper_limit = False
+	upper_limit_val = None
+
+	lower_limit = False
+	lower_limit_val = None
+
+	pagination_length = 3
 
 	request_url = "https://"+Configuration.subdomain+"/api/v2/tickets.json?per_page=" + str(Configuration.per_page_tickets) + "&page=" + page_value + "&sort_by=id"
 	headers = {
@@ -22,7 +29,8 @@ def index():
 
 	try:
 		response = requests.get(request_url, headers=headers)
-	
+		page_value = int(page_value)
+
 		if not response.ok:
 			flash("Credentials Incorrect. Access denied.","error")
 			flag = False
@@ -33,7 +41,18 @@ def index():
 			count_tickets = response['count']
 
 			if count_tickets != 0:
-				total_pages = ceil(count_tickets/Configuration.per_page_tickets)
+				total_pages = ceil(count_tickets / Configuration.per_page_tickets)
+				lower_limit_val = page_value
+				upper_limit_val = page_value + pagination_length - 1
+
+				if lower_limit_val != 1:
+					lower_limit = True
+
+				if upper_limit_val > total_pages:
+					upper_limit_val = total_pages
+				else:
+					upper_limit = True
+				page_range = range(lower_limit_val, upper_limit_val + 1)
 			else:
 				total_pages = 0
 			page_tickets = len(response['tickets'])
@@ -44,7 +63,12 @@ def index():
 								flag=flag, 
 								total_tickets=count_tickets, 
 								page_tickets=page_tickets,
-								total_pages=range(1, total_pages+1)), 200
+								upper_limit=upper_limit,
+								lower_limit=lower_limit,
+								upper_limit_val=upper_limit_val,
+								lower_limit_val=lower_limit_val,
+								page_range=page_range), 200
+
 	except json.decoder.JSONDecodeError:
 		return render_template("index.html", 
 							   valid_response=False), 200
